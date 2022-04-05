c7b6427088ed2355dfe1690291f2d6d36a29bd2e
solnor <solve.norsterud@gmail.com>
2022-04-05 22:46:18 +0200

    Cleaned up a bit

--- network/peers/peers.go
+++ network/peers/peers.go
@@ -3,6 +3,7 @@ package peers
 import (
 	nodeConfig "driver/config"
 	"elevator/fsm"
+	"fmt"
 	"network/bcast"
 	"sort"
 	"time"
@@ -67,6 +68,7 @@ func Receiver(port int, thisId string, peerUpdateCh chan<- PeerUpdate, nodeUpdat
 			nodeConfig.KnownNodesMutex.RLock()
 			node := nodeConfig.KnownNodesTable[string(id)]
 			nodeConfig.KnownNodesMutex.RUnlock()
+			fmt.Println(node.Elevator.Requests)
 			if node != nil {
 				if node.Id == thisId {
 					nodeConfig.KnownNodesMutex.Lock()
